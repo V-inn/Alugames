cf826798ae69c23d534fd8fd08b20065f2165103
Vin <93566073+V-inn@users.noreply.github.com>
2024-07-16 14:37:22 -0300

    Implemented logic

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const rentedClass = "dashboard__item__img--rented"
+const returnClass = "dashboard__item__button--return"
+
+function alterarStatus(id){
+    //Pega o item com o identificador "game-id"
+    var item = document.getElementById("game-"+id);
+    var status = "available"
+
+    for (const child of item.children) {
+        if(child.tagName == "DIV"){
+            if(child.classList.contains(rentedClass)){
+                child.classList.remove(rentedClass);
+                status = "available";
+            }else{
+                child.classList.add(rentedClass);
+                status = "rented";
+            }
+        }else if(child.tagName == "A"){
+            if(status == "available"){
+                child.classList.remove(returnClass);
+                child.innerHTML = "Alugar";
+            }else{
+                child.classList.add(returnClass);
+                child.innerHTML = "Devolver";
+            }
+        }
+    }
+}
